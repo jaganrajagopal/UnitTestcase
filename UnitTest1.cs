75cb1e4dcb8dd31673f5beecbf0aea05f50956d1
Jagan <jaganrajagopal@outlook.com>
2019-05-12 19:41:37 +0530

    Unit Test for calculation process
    Calculation process

---
+++ UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Jenkinsdotnetprojects;
+
+namespace CalculationNunit
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void AddTestCase()
+        {
+            CalculationProcess myCalc = new CalculationProcess();
+
+            Assert.AreEqual(myCalc.Add(1, 2), 3);
+        }
+        [TestMethod]
+        public void SubtractTestCase()
+        {
+            CalculationProcess myCalc = new CalculationProcess();
+
+            Assert.AreEqual(myCalc.subtract(3, 1), 2);
+        }
+    }
+}
